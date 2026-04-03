3b344ba30203aac5971ec1d378a594c5dc760dc4
ALIYU GAMBO ABUBAKAR <aliyu22mof@gmail.com>
2026-04-03 21:56:53 +0530

    feat: add express.json and cors middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // server.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const connectDB = require('./db');
 connectDB();
